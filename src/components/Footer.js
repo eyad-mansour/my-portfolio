32abd7a690027f7c6b4873a9c8041d2a5fb9eee7
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-07-29 14:56:54 +0300

    removed the second language added the project needed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,14 +44,7 @@ class Footer extends Component {
                   )
                 }
                 style={{ display: "inline" }}
-              >
-                <span
-                  className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
-                  data-inline="false"
-                  id={window.$secondaryLanguageIconId}
-                ></span>
-              </div>
+              ></div>
             </div>
             <div className="col">
               <div className="copyright py-4 text-center">
